2649d3e03d907a8ffe5a76bab554b59378ef6bb1
No1mann <no1trevormann@gmail.com>
2022-12-01 22:48:36 -0500

    More cache fixes

--- functions/api/current.js
+++ functions/api/current.js
@@ -2,51 +2,58 @@ async function getRealTimeData(request, spreedsheet, query, year){
 
     const cache = await caches.open("jingle-jam-current");
     
-    const cacheKey = new Request(request.url);
+    const cacheKey = new Request(new URL(request.url).toString());
 
     let cacheResponse = await cache.match(cacheKey);
     
     if (!cacheResponse) {
         console.log("Cache not hit");
 
-        let sheetResponse = await fetch(`https://docs.google.com/spreadsheets/d/${spreedsheet}/gviz/tq?tq=${query}&headers=0&sheet=${year} Data Set&tqx=out:csv`);
+        cacheResponse = await fetch(`https://docs.google.com/spreadsheets/d/${spreedsheet}/gviz/tq?tq=${query}&headers=0&sheet=${year} Data Set&tqx=out:csv`);
     
-        let csv = await sheetResponse.text();
-    
-        let rows = csv.split('\n');
+        let responseToCache = new Response(cacheResponse.body, cacheResponse);
 
-        let results = [];
-        for(let i = 1; i < rows.length; i++){
-            let row = rows[i];
-            let rawData = row.split('","');
-    
-            let data = []
-            for(let entry of rawData){
-                data.push(entry.replaceAll('"', '').replaceAll(',', '').replaceAll('$', '').replaceAll('£', ''));
-            }
-    
-            results.push({
-                timestamp: data[0],
-                amountPounds: parseFloat(data[1]),
-                amountDollars: parseFloat(data[2]),
-                year: year
-            });
-        }
-    
-        let response = new Response(JSON.stringify(results));
+        responseToCache.headers.append('Cache-Control', 's-maxage=1200');
 
-        response.headers.append('content-type', 'application/json;charset=UTF-8');
-        response.headers.append('Access-Control-Allow-Origin', '*');
-        response.headers.append('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
-        response.headers.append('Access-Control-Max-Age', '86400');
-        response.headers.append('Allow', 'GET, HEAD, OPTIONS');
-        response.headers.append('Cache-Control', 's-maxage=1200');
-
-        return response;
+        cache.put(cacheKey, responseToCache.clone());
     }
-    console.log("Cache hit");
+    else{
+        console.log("Cache hit");
+    }
 
-    return cacheResponse;
+    let csv = await cacheResponse.text();
+
+    let rows = csv.split('\n');
+
+    let results = [];
+    for(let i = 1; i < rows.length; i++){
+        let row = rows[i];
+        let rawData = row.split('","');
+
+        let data = []
+        for(let entry of rawData){
+            data.push(entry.replaceAll('"', '').replaceAll(',', '').replaceAll('$', '').replaceAll('£', ''));
+        }
+
+        results.push({
+            timestamp: data[0],
+            amountPounds: parseFloat(data[1]),
+            amountDollars: parseFloat(data[2]),
+            year: year
+        });
+    }
+
+    let response = new Response(JSON.stringify(results));
+
+    response.headers.append('content-type', 'application/json;charset=UTF-8');
+    response.headers.append('Access-Control-Allow-Origin', '*');
+    response.headers.append('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+    response.headers.append('Access-Control-Max-Age', '86400');
+    response.headers.append('Allow', 'GET, HEAD, OPTIONS');
+
+    console.log(response);
+
+    return response;
 }
   
 async function getOldData(){
@@ -76,13 +83,22 @@ export async function onRequest(context) {
     try{
         let results = await getRealTimeData(context.request, spreadsheet, query, year);
 
-        if(results.length <= 2)
+        if(results.length <= 4)
             throw "Error";
 
         return results;
     }
-    catch{
-        return await getOldData();
+    catch (e){
+        console.log(e);
+        return new Response(await getOldData(), {
+            headers: {
+            "content-type": "application/json;charset=UTF-8",
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+            'Access-Control-Max-Age': '86400',
+            'Allow': 'GET, HEAD, OPTIONS'
+            }
+        });
     }
   }
      
